3550bcf71c96cf5edb65929ac6b74c8f17a7bbbf
Max <maxim.nechai@gmail.com>
2017-06-20 19:50:09 +0300

    Calculating the number of hosts per subnet

--- subnetcalc.py
+++ subnetcalc.py
@@ -73,7 +73,19 @@ def subnet_calc():
 
         decimal_mask = "".join(mask_octets_padded)
         #print decimal_mask   #Example: for 255.255.255.0 => 11111111111111111111111100000000
-   
+        
+
+
+        #Counting host bits in the mask and calculating number of hosts/subnet
+        no_of_zeros = decimal_mask.count("0")
+        no_of_ones = 32 - no_of_zeros
+        no_of_hosts = abs(2 ** no_of_zeros - 2) #return positive value for mask /32
+        
+        #print no_of_zeros
+        #print no_of_ones
+        #print no_of_hosts
+        
+    
     except KeyboardInterrupt:
         print "\n\nProgram aborted by user. Exiting...\n"
         sys.exit()
