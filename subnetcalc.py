f698b808c76c1e47c8b98ab6497e10fbe588a4be
Max <maxim.nechai@gmail.com>
2017-06-20 22:04:29 +0300

    Generating a random IP address in the subnet

--- subnetcalc.py
+++ subnetcalc.py
@@ -182,6 +182,40 @@ def subnet_calc():
         print "Mask bits: %s" % no_of_ones
         print "\n"
         
+        #Part 4.
+        
+        #Generation of random IP in subnet
+        while True:
+            generate = raw_input("Generate random ip address from subnet? (y/n)")
+            
+            if generate == "y":
+                generated_ip = []
+                
+                #Obtain available IP address in range, based on the difference between octets in broadcast address and network address
+                for indexb, oct_bst in enumerate(bst_ip_address):  #enumerate return index and value at the same time
+                    #print indexb, oct_bst
+                    for indexn, oct_net in enumerate(net_ip_address):
+                        #print indexn, oct_net
+                        if indexb == indexn:
+                            if oct_bst == oct_net:
+                                #Add identical octets to the generated_ip list
+                                generated_ip.append(oct_bst)
+                            else:
+                                #Generate random number(s) from within octet intervals and append to the list
+                                generated_ip.append(str(random.randint(int(oct_net), int(oct_bst))))
+                
+                #IP address generated from the subnet pool
+                #print generated_ip
+                y_iaddr = ".".join(generated_ip)
+                #print y_iaddr
+                
+                print "Random IP address is: %s" % y_iaddr
+                print "\n"
+                continue
+                
+            else:
+                print "Ok, bye!\n"
+                break
 	
 
     except KeyboardInterrupt:
