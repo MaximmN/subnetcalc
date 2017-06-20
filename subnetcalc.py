d8b0b562e5169cb00a5937d71078c38e8cc91c17
Max <maxim.nechai@gmail.com>
2017-06-20 17:46:59 +0300

    IP adress input and validity check

--- subnetcalc.py
+++ subnetcalc.py
@@ -2,3 +2,24 @@
 
 import random
 import sys
+
+def subnet_calc():
+	try:
+		print "\n"
+
+		# Checking IP address validity
+		while True:
+			# asking the user for IP address
+			ip_address = raw_input("Enter an IP address: ")
+
+			# Checking octets
+			# return a list of users IP address
+			a = ip_address.split('.')
+
+			if (len(a) == 4) and (1 <= int(a[0]) <= 223) and (int(a[0]) != 127) and (int(a[0]) != 169 or int(a[1]) != 254) and (0 <= int(a[1]) <= 255 and 0 <= int(a[2]) <=255 and 0 <= int(a[3]) <= 255);
+				break
+
+			else:
+				print "\nThe IP address is INVALID! Please retry!\n"
+				# redirect the user to the top of while loop
+				continue
